67e8e00a7d25e044a61281c6246bb1c8dcd0b1b2
SHarpreet89 <S.harpreet@me.com>
2024-07-30 22:37:41 +1000

    Updated Server.js Paths

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-
-//const api = require('./routes/index.js');
+const notesDB = require('./db/db.json')
 
 const PORT = process.env.port || 3001;
 
@@ -10,10 +9,26 @@ const app = express();
 // Middleware to serve static files
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//Route to send the index.html
+
+
 // Route to serve notes.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
+
+// //Route t send the Notes Data when requested
+app.get('/api/notes', (req, res) => {
+  res.json(notesDB)
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 
 // Start the server
 app.listen(PORT, () => {
