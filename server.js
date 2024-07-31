ffcfd389f798def655dc712f169378acaa2d31e3
SHarpreet89 <S.harpreet@me.com>
2024-07-31 23:20:27 +1000

    Updated Server.js to Create and Delete nodes and ensure they persist in the database

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
 const notesDB = require('./db/db.json')
+const fs = require('fs');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const uuid = () => {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
 
 // Middleware to serve static files
 app.use(express.static('public'));
@@ -19,12 +26,45 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-// //Route t send the Notes Data when requested
+// Route to send the Notes Data when requested
 app.get('/api/notes', (req, res) => {
   res.json(notesDB)
 });
 
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid(); // Generate and assign a unique ID to the new note
+  notesDB.push(newNote);
 
+  fs.writeFile('./db/db.json', JSON.stringify(notesDB, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to save the note' });
+    }
+    res.json(notesDB);
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const noteIndex = notesDB.findIndex(note => note.id === noteId);
+
+  if (noteIndex !== -1) {
+    notesDB.splice(noteIndex, 1);
+
+    fs.writeFile('./db/db.json', JSON.stringify(notesDB, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to delete the note' });
+      }
+      res.json({ message: 'Note deleted successfully' });
+    });
+  } else {
+    res.status(404).json({ error: 'Note not found' });
+  }
+});
+
+// Route to capture all invalid paths and revert to index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
